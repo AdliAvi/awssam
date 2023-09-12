7cd16a722ce99274ac074ff86908b728290092fe
AviorX <avioradli@gmail.com>
2023-09-12 15:57:21 -0400

    check

--- lambda.py
+++ lambda.py
@@ -2,13 +2,13 @@ import requests
 import json
 
 def handler(event, context):
-    response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
-    res = {
-        "event": event,
-        "output": response.json(),
-        "context": context
-    }
-    print(res)
-    print("Hello World")
+    #response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+    #res = {
+    #    "event": event,
+    #    "output": response.json(),
+    #    "context": context
+    # }
+    # print(res)
+    print("Hello World2")
 
     return None
