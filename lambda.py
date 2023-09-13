3fd76d72dcd942c55a61def543b1ce92251f7615
AviorX <avioradli@gmail.com>
2023-09-12 21:02:57 -0400

    commit delete stack

--- lambda.py
+++ lambda.py
@@ -9,6 +9,6 @@ def handler(event, context):
     #    "context": context
     # }
     # print(res)
-    print("Hello World please work")
+    print("Hello World please work after deleting")
 
     return None
